1108d71eed666661026e917f87faaa7d1f4c006a
Hari Om Tiwari <hariom.codes@gmail.com>
2022-01-23 18:38:44 +0530

    add login page

--- routes/users.js
+++ routes/users.js
@@ -8,18 +8,27 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-router.post('/register', catchAsync(async (req,res) => {
+router.post('/register', catchAsync(async (req, res) => {
     try {
-    const {email,username,password} = req.body;
-    const user = new User({email, username});
-    const registeredUser = await User.register(user,password);
-    } catch(e) {
-        req.flash('error',e.message);
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+    } catch (e) {
+        req.flash('error', e.message);
         res.redirect('register');
     }
     console.log(registeredUser);
     req.flash('success', 'Welcome to Yelp Camp!');
     res.redirect('/campgrounds');
 }))
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+})
+router.post('/login', passport.authenticate('local', {failureFlash:true, failureRedirect: '/login'}), (req, res) => {
+    req.flash('success', 'welcome back');
+    res.redirect('/campgrounds');
+})
+
 
 module.exports = router;

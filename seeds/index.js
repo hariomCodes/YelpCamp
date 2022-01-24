3a86cb0123464b0268ea888884285be851bf3bbe
Hari Om Tiwari <hariom.codes@gmail.com>
2022-01-24 15:40:54 +0530

    add author to the show page

--- seeds/index.js
+++ seeds/index.js
@@ -22,7 +22,7 @@ const seedDB = async () => {
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             //YOUR USER ID
-            // author: '5f5c330c2cd79d538f2c66d9',
+            author: '61ed4f0d7bb491c991edb690',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',

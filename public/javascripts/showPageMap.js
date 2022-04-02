287894107a391a9897cc4cdc8168f130bf5effa6
Hari Om Tiwari <hariom.codes@gmail.com>
2022-04-02 13:47:11 +0530

    add map

---
+++ public/javascripts/showPageMap.js
@@ -0,0 +1,14 @@
+mapboxgl.accessToken = mapToken;
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: campground.geometry.coordinates,
+  zoom: 8,
+});
+
+new mapboxgl.Marker()
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(`<h3>${campground.title}</h3><p>${campground.location}</p>`)
+  )
+  .addTo(map);
